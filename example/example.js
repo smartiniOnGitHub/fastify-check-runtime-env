47671a7351956201260d3fc53214758e53b8b3f8
Sandro Martini <sandro.martini@gmail.com>
2019-06-07 00:01:54 +0200

    add minimal example and related static home page; cleanup asap

---
+++ example/example.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict'
+
+const fastify = require('fastify')()
+
+const k = {
+  protocol: 'http',
+  address: '0.0.0.0',
+  port: 3000
+}
+
+// const nodeVersion = process.version
+const engines = require('../package.json').engines
+
+// register plugin with some options (as a sample)
+fastify.register(require('../src/plugin'), {
+  nodeVersionCheckAtStartup: true,
+  nodeVersionExpected: engines.node
+  // nodeVersionExpected: '>=16.0.0' // sample failing test
+  // onNodeVersionMismatch: 'warning' // log a warning
+  // onNodeVersionMismatch: 'exception' // throw an exception // same as default
+  // onNodeVersionMismatch: 'exit' // exit the process
+  // TODO: cleanup commented code, and move in other example/s ... wip
+})
+
+// example to handle a sample home request to serve a static page, optional here
+fastify.get('/', function (req, reply) {
+  const path = require('path')
+  const scriptRelativeFolder = path.join(__dirname, path.sep)
+  const fs = require('fs')
+  const stream = fs.createReadStream(path.join(scriptRelativeFolder, 'home.html'))
+  reply.type('text/html; charset=utf-8').send(stream)
+})
+
+fastify.listen(k.port, k.address, (err, address) => {
+  if (err) throw err
+  console.log(`Server listening on ${address}`)
+})
+
+fastify.ready(() => {
+  const routes = fastify.printRoutes()
+  console.log(`Available Routes:\n${routes}`)
+})
